613da01cf5a48e090b5e491d66b95060f12177c2
znicholas zhen <zhen_002@qq.com>
2012-04-08 13:37:11 +0800

    修改Entity名称

--- src/main/java/com/yunbo/oflow/model/ProcessDefinition.java
+++
@@ -1,5 +0,0 @@
-package com.yunbo.oflow.model;
-
-public class ProcessDefinition {
-
-}
---
+++ src/main/java/com/yunbo/oflow/model/ProcessDefinitionEntity.java
@@ -0,0 +1,16 @@
+package com.yunbo.oflow.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "OFLOW_PROCESS_DEFINITION")
+public class ProcessDefinitionEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 802732898815902642L;
+}
